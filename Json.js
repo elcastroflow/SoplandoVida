38e04fdf67a8f414812de9d238e6868d1e70d5e0
Juan Jose castro <juan.castro@ctc.edu.do>
2016-08-29 04:38:48 -0400

    LogoIglesias
    Coloque los logos de las iglesias y parte de lo que sera la descripcion

---
+++ Json.js
@@ -0,0 +1,11 @@
+$(document).ready({
+    var miJson = new Object();
+miJson.name = "John";
+miJson.age = 12;
+miJson.pets = ["cat", "dog"];
+    var myString = JSON.stringify(miJson);  
+}); 
+
+
+
+
